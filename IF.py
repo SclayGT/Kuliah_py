52348fc099faef8219b625b6fb5407eabb0c702b
SclayGT <ronywijaya825@gmail.com>
2025-09-03 15:46:22 +0700

    first commit

--- IF.py
+++ IF.py
@@ -1,5 +1,27 @@
-if True:
-    print("Ini selalu dicetak")
+Nilai = int(input("Masukkan Nilai: "))
+ 
+if Nilai >= 90:
+    print("Nilai Anda A")
+elif Nilai >= 80:
+    print("Nilai Anda B")
+elif Nilai >= 60:
+    print("Nilai Anda C")
+elif Nilai >= 40:
+    print("Nilai Anda D")
 else:
-    print("Ini tidak dicetak")
-    print("Ini bukan termasuk bagian dari IF")
+    print("Nilai Anda E")
+    
+print(12*"=")    
+print("IF TANPA ELIF")    
+print(12*"=")    
+    
+if Nilai >= 90:
+    print("Nilai Anda A")
+if Nilai >= 80:
+    print("Nilai Anda B")
+if Nilai >= 80:
+    print("Nilai Anda C")
+if Nilai >= 80:
+    print("Nilai Anda D")
+else:
+    print("Nilai Anda E")
